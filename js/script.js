0bc3f8f096549cc2d59354fb7ace84d46238bd63
Mostafa <Xmwo3i@outlook.com>
2025-03-06 09:38:51 -0500

    changesss

--- js/script.js
+++ js/script.js
@@ -8,32 +8,59 @@ window.addEventListener("load", function(event){
     const rectanglebutton = this.document.getElementById("rectangle");
     const circlebutton = this.document.getElementById("circle");
     const linebutton = this.document.getElementById("line");
+    const colour = this.document.getElementById("colour");
+    let colourSelected;
+    const shapeButtons = this.document.querySelectorAll("button"); 
+    let shapeSelected; 
 
     // add event listener to the canvas
     c.addEventListener("click", function(event){
 
     })
+    colour.addEventListener("change", (e)=>{
+        colourSelected = colour.value; 
+    })
+    shapeButtons.addEventListener("click", (e)=>{
+        if (shapeSelected === "rectangle"){
+            c.addEventListener("click", (e)=>{
+                
+            })
+        }
+        if (shapeSelected === "triangle"){
+
+        }
+        if (shapeSelected === "circle"){}
+
+        if (shapeSelected === "line"){
+        }
+
+        
+    })
 
     // add event listeners to buttons
 
     rectanglebutton.addEventListener("click", (e)=>{
-        let rectangle = new Rectangle(10, 10, 10, 10, "black");
+        let rectangle = new Rectangle(10, 10, 10, 10, colourSelected);
         rectangle.draw();
+        shapeSelected = "rectangle"; 
     });
 
     circlebutton.addEventListener("click", (e)=>{
         Circle.draw();
+        shapeSelected = "circle"
     }); 
     trianglebutton.addEventListener("click",(e)=>{
         Triangle.draw(); 
+        shapeSelected = "triangle"
     });
     linebutton.addEventListener("click", (e)=>{
         Line.draw();
+        shapeSelected = "line"; 
     })
 
     // triangle, circle, rectangle, line, brush
-    class Shape {
-        constructor(colour) {
+    class Shape{
+        constructor(colour){
             this.colour = colour;
         }
     }
@@ -47,17 +74,17 @@ window.addEventListener("load", function(event){
             this.height = height;
         }
         draw() {
-            ctx.fillStyle = colour;
+            ctx.fillStyle = this.colour;
             ctx.fillRect(this.x, this.y, this.width, this.height); 
         }
     }
 
     class Circle extends Shape  {
         constructor(x, y, radius, colour) {
+            super(colour);
             this.x = x;
             this.y = y;
             this.radius = radius;
-            super(colour);
         }
         draw() {
             ctx.beginPath();
@@ -69,13 +96,13 @@ window.addEventListener("load", function(event){
 
     class Line extends Shape {
         constructor(xStart, yStart, xEnd, yEnd, strokeWidth, colour) {
+            super(colour);
             this.xStart = xStart;
             this.yStart = yStart;
             this.xEnd = xEnd;
             this.yEnd = yEnd;
             this.strokeWidth = strokeWidth;
 
-            super(colour);
         }
         draw() {
             ctx.beginPath();
